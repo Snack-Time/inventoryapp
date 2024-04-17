29873d9e5f5dd3f2e3320bc9af311b47091c1f96
Snack-Time <wmcnally1226@gmail.com>
2024-04-17 02:18:03 -0400

    work on esrb delete and add functionality

--- controllers/ESRBController.js
+++ controllers/ESRBController.js
@@ -1,6 +1,7 @@
 const ESRB = require("../models/ESRB")
 const VideoGame = require("../models/videogame")
 const asyncHandler = require("express-async-handler")
+const { body, validationResult } = require("express-validator")
 
 // Display list of all companies.
   exports.esrb_list = asyncHandler(async (req, res, next) => {
@@ -31,22 +32,86 @@ const asyncHandler = require("express-async-handler")
   
   // Display esrb create form on GET.
   exports.esrb_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: esrb create GET");
+    res.render("esrb_form", { title: "Create Rating" })
   });
   
   // Handle esrb create on POST.
-  exports.esrb_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: esrb create POST");
-  });
+  exports.esrb_create_post = [
+    body('name', 'Rating name must be at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+    body('acronym', 'Rating acronym must be between 1 to 3 characters')
+    .trim()
+    .isLength({ max: 3, min: 1 })
+    .escape(),
+    body('desc', 'Description must be at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+    asyncHandler(async (req, res, next) => {
+      const errors = validationResult(req);
+      const esrb = new ESRB({ name: req.body.name, acronym: req.body.acronym, desc: req.body.desc });
+
+      if (!errors.isEmpty()) {
+        res.render("esrb_form", {
+          title: "Create Rating",
+          esrb: esrb,
+          errors: errors.array(),
+        });
+        return
+      } 
+      else {
+        const ratingExists = await ESRB.findOne({ name: req.body.name }).collation({ locale: "en", strength: 2 }).exec()
+        if (ratingExists) {
+          res.redirect(ratingExists.url);
+        }
+        else {
+          await esrb.save();
+          res.redirect(esrb.url)
+        }
+      }
+    }),
+  ]
   
   // Display esrb delete form on GET.
   exports.esrb_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: esrb delete GET");
+    const [esrb, gamesRated] = await Promise.all([
+      ESRB.findById(req.params.id).exec(),
+      VideoGame.find( { ESRB: req.params.id }, "name").exec(),
+    ]);
+  
+    if (esrb === null) {
+      res.redirect('/catalog/ESRBRatings');
+    }
+  
+    res.render("esrb_delete", {
+      title: "Delete Rating",
+      esrb: esrb,
+      games_with_rating: gamesRated,
+    })
   });
   
   // Handle esrb delete on POST.
   exports.esrb_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: esrb delete POST");
+    const [esrb, gamesRated] = await Promise.all([
+      ESRB.findById(req.params.id).exec(),
+      VideoGame.find( { ESRB: req.params.id }, "name").exec(),
+    ]);
+
+    if (gamesRated.length > 0) {
+      res.render("esrb_delete", {
+        title: "Delete Rating",
+        esrb: esrb,
+        games_with_rating: gamesRated,
+      });
+      return;
+    }
+    else {
+      await ESRB.findByIdAndDelete(req.body.ratingid);
+      res.redirect('/catalog/ESRBRatings')
+    }    
   });
   
   // Display esrb update form on GET.
