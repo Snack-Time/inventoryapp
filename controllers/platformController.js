d5490df97562f1143140b07b0310588cfbe19a0b
Snack-Time <wmcnally1226@gmail.com>
2024-04-18 00:18:54 -0400

    add platform adding functionality

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -1,6 +1,8 @@
 const Platform = require("../models/platform");
-const VideoGame = require("../models/videogame")
+const VideoGame = require("../models/videogame");
+const Company = require("../models/company");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all platform.
 exports.platform_list = asyncHandler(async (req, res, next) => {
@@ -34,13 +36,56 @@ exports.platform_detail = asyncHandler(async (req, res, next) => {
 
 // Display platform create form on GET.
 exports.platform_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: platform create GET");
+  const allDevelopers = await Company.find().sort({ name: 1 }).exec()
+
+  res.render("platform_form", {
+    title: "Create Platform",
+    developers: allDevelopers,
+  })
 });
 
 // Handle platform create on POST.
-exports.platform_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: platform create POST");
-});
+exports.platform_create_post = [
+  body('name', 'Platform name must be less than 100 characters. (Why are you trying to do that anyway???)')
+  .trim()
+  .isLength({ max: 100 })
+  .escape(),
+  body('release_date', 'Release Date Error')
+  .isDate()
+  .optional( {values: "falsy"})
+  .escape(),
+  body('developer', 'Pick a valid company')
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const platform = new Platform({ name: req.body.name, release_date: req.body.release_date, developer: req.body.developer });
+
+    if (!errors.isEmpty()) {
+      const allDevelopers = await Company.find().sort({ name: 1 }).exec()
+
+      res.render("platform_form", {
+        title: "Create Platform",
+        platform: platform,
+        developers: allDevelopers,
+        errors: errors.array(),
+      });
+      return
+    } 
+    else {
+      const platformExists = await Platform.findOne({ name: req.body.name }).collation({ locale: "en", strength: 2 }).exec()
+      if (platformExists) {
+        res.redirect(platformExists.url);
+      }
+      else {
+        await platform.save();
+        res.redirect(platform.url)
+      }
+    }
+  }),
+];
 
 // Display platform delete form on GET.
 exports.platform_delete_get = asyncHandler(async (req, res, next) => {
